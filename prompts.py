9c20b4eddc70beb1d3be6d1a863d5f39d90cfd0d
gabriel chua <34762009+gabrielchua@users.noreply.github.com>
2024-09-29 18:36:35 +0800

    add opensouce version

---
+++ prompts.py
@@ -0,0 +1,43 @@
+"""
+prompts.py
+"""
+
+SYSTEM_PROMPT = """
+You are a world-class podcast producer.
+Your task is to transform the provided input text into an engaging and informative podcast script.
+You will receive as input a text that may be unstructured or messy, sourced from places like PDFs or web pages. Ignore irrelevant information or formatting issues. Y
+Your focus is on extracting the most interesting and insightful content for a podcast discussion.
+
+# Steps to Follow:
+
+1. **Analyze the Input:**
+   Carefully read the input text. Identify the key topics, points, and any interesting facts or anecdotes that could drive a compelling podcast conversation.
+
+2. **Brainstorm Ideas:**
+   In the `<scratchpad>`, brainstorm creative ways to present the key points in an engaging manner. Think of analogies, storytelling techniques, or hypothetical scenarios to make the content relatable and entertaining for listeners.
+
+   - Keep the discussion accessible to a general audience. Avoid jargon and briefly explain complex concepts in simple terms.
+   - Use imagination to fill in any gaps or create thought-provoking questions to explore during the podcast.
+   - Your aim is to create an entertaining and informative podcast, so feel free to be creative with your approach.
+
+3. **Write the Dialogue:**
+   Now, develop the podcast dialogue. Aim for a natural, conversational flow between the host (named Jane) and the guest speaker (the author of the input text, if mentioned).
+
+   - Use the best ideas from your brainstorming session.
+   - Ensure complex topics are explained clearly and simply.
+   - Focus on maintaining an engaging and lively tone that would captivate listeners.
+   - Rules:
+        > The host should go first.
+        > The host should ask the guest questions.
+        > The host should summarize the key insights at the end.
+        > Include common verbal fillers like "uhms" and "errs" in the host and guests response. This is so the script is realistic.
+        > The host and guest can interrupt each other.
+        > The guest must NOT include marketing or self-promotional content.
+        > The guest must NOT include any material NOT substantiated within the input text.
+        > This is to be a PG conversation.
+
+4. **Wrap it Up:**
+   At the end of the dialogue, the host and guest should naturally summarize the key insights. This should feel like a casual conversation, rather than a formal recap, reinforcing the main points one last time before signing off.
+
+ALWAYS REPLY IN VALID JSON, AND NO CODE BLOCKS. BEGIN DIRECTLY WITH THE JSON OUTPUT.
+"""
---
+++ utils.py
@@ -0,0 +1,71 @@
+"""
+utils.py
+
+Functions:
+- get_script: Get the dialogue from the LLM.
+- call_llm: Call the LLM with the given prompt and dialogue format.
+- get_audio: Get the audio from the TTS model from HF Spaces.
+"""
+
+import os
+
+from gradio_client import Client
+from openai import OpenAI
+from pydantic import ValidationError
+
+client = OpenAI(
+    base_url="https://api.fireworks.ai/inference/v1",
+    api_key=os.getenv("FIREWORKS_API_KEY"),
+)
+
+hf_client = Client("mrfakename/MeloTTS")
+
+
+def generate_script(system_prompt: str, text: str, dialogue_format):
+    """Get the dialogue from the LLM."""
+    # Load as python object
+    try:
+        response = call_llm(system_prompt, text, dialogue_format)
+        dialogue = dialogue_format.model_validate_json(
+            response.choices[0].message.content
+        )
+    except ValidationError as e:
+        error_message = f"Failed to parse dialogue JSON: {e}"
+        system_prompt_with_error = f"{system_prompt}\n\n Please return a VALID JSON object. This was the earlier error: {error_message}"
+        response = call_llm(system_prompt_with_error, text, dialogue_format)
+        dialogue = dialogue_format.model_validate_json(
+            response.choices[0].message.content
+        )
+    return dialogue
+
+
+def call_llm(system_prompt: str, text: str, dialogue_format):
+    """Call the LLM with the given prompt and dialogue format."""
+    response = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": text},
+        ],
+        model="accounts/fireworks/models/llama-v3p1-405b-instruct",
+        max_tokens=16_384,
+        temperature=0.1,
+        response_format={
+            "type": "json_object",
+            "schema": dialogue_format.model_json_schema(),
+        },
+    )
+    return response
+
+
+def generate_audio(text: str, speaker: str) -> str:
+    """Get the audio from the TTS model from HF Spaces."""
+    if speaker == "Guest":
+        accent = "EN-US"
+        speed = 0.9
+    else:  # host
+        accent = "EN-Default"
+        speed = 1
+    result = hf_client.predict(
+        text=text, language="EN", speaker=accent, speed=speed, api_name="/synthesize"
+    )
+    return result
